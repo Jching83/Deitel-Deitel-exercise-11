daa44052f8a8fec1fbaabb46b68267245560e8e0
Jason Ching <jching83@gmail.com>
2021-03-05 06:42:20 -0600

    Add files via upload
    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+ * ****************************************************************
+ * 2.17 (Printing) Write a program that prints the numbers 1 to 4 *
+ * on the same line with each pair of adjacent numbers separated  *
+ * by one space. Do this several ways:                            *
+ * A. Using one statement with one stream insertion operator.     *
+ * B. Using one statement with four stream insertion              *
+ * operators.                                                     *
+ * C. Using four statements.                                      *
+ * ****************************************************************
+ */
+
+#include <iostream> //enable input and output
+using namespace std; // using standard library namespace
+
+//function main begins
+int main() {
+    cout << "1 2 3 4" << endl; // Statement to print the number 1 through 4 on one line
+    return 0;
+} //end function main
